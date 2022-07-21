c99f6ecf5bbe1a60e05f558027dfed9c0b7850e0
Treboder <robert.kohn@gmx.net>
2022-07-21 23:04:31 +0200

    Stage 2/5: Virtual card deck (done without beautification)

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -1,27 +1,98 @@
-package indigo
+import kotlin.random.Random
 
 fun main() {
 
-        // https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
+        do {
+                print("Choose an action (reset, shuffle, get, exit): \n> ")
+                var command = readLine()!!
+                when(command) {
+                        "reset" -> Deck.reset()
+                        "shuffle" -> Deck.shuffle()
+                        "get" -> Deck.get()
+                        "print" -> Deck.printCardList(Deck.cards)
+                        "exit" -> break
+                        else -> println("Wrong action.")
+                }
+        } while(command != "exit")
+        println("Bye")
 
-        for(rank in RANKS.values()) {
-                print("${rank.Symbol} ")
+}
+
+
+
+object Deck {
+
+        var cards = mutableListOf<Card>()
+
+        init {
+                initialize()
         }
-        println("\n")
 
-        for(suit in SUITS.values()) {
-                print("${suit.Symbol} ")
+        fun initialize() {
+                for(suit in SUITS.values())
+                        for(rank in RANKS.values())
+                                cards.add(Card(suit, rank))
         }
-        println("\n")
 
-        for(suit in SUITS.values())
-                for(rank in RANKS.values()) {
-                        print("${rank.Symbol}${suit.Symbol} ")
+        fun reset() {
+                cards = mutableListOf<Card>()
+                initialize()
+                println("Card deck is reset.")
+        }
+
+        fun shuffle() {
+                var rnd = Random
+                repeat(52) {
+                        var cardIndex1 = rnd.nextInt(0,52)
+                        var cardIndex2 = rnd.nextInt(0,52)
+                        var swap = cards[cardIndex1]
+                        cards[cardIndex1] =  cards[cardIndex2]
+                        cards[cardIndex2] =  swap
+                }
+                println("Card deck is shuffled.")
+        }
+
+        fun get() {
+                var rnd = Random
+                print("Number of cards: \n> ")
+                var numberOfCardsToGet = 0
+                try {
+                        numberOfCardsToGet = readLine()!!.toInt()
+                } catch (e:Exception) {
+                        println("Invalid number of cards.")
+                        return
                 }
 
+                var selectedCards = mutableListOf<Card>()
+                if(numberOfCardsToGet in 1..52) {
+                        if(numberOfCardsToGet <= cards.count()) {
+                                while(selectedCards.count() < numberOfCardsToGet) {
+                                        var index = rnd.nextInt(0,cards.count())
+                                        selectedCards.add(cards[index])
+                                        cards.removeAt(index)
+                                }
+                        }
+                        else
+                                println("The remaining cards are insufficient to meet the request.")
+                }
+                else
+                        println("Invalid number of cards.")
 
+                printCardList(selectedCards)
+        }
 
+        fun printCardList(cardListToPrint:MutableList<Card>) {
+                for(card in cardListToPrint)
+                        card.print()
+                println()
+        }
+}
 
+class Card(var suit:SUITS, var rank:RANKS) {
+
+        fun print() {
+                print("${rank.Symbol}${suit.Symbol} ")
+        }
 }
 
 enum class RANKS(val Symbol:String, Rank: Int) {
@@ -45,4 +116,23 @@ enum class SUITS(val CodePoint: Char, val Symbol:String, Rank: Int) {
         SPADE('\u2660', Character.toString('\u2660'), 1),
         HEART('\u2665', Character.toString('\u2665'), 2),
         DIAMOND('\u2666', Character.toString('\u2666'), 3)
+}
+
+fun stage1() {
+        // https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
+
+        for(rank in RANKS.values()) {
+                print("${rank.Symbol} ")
+        }
+        println("\n")
+
+        for(suit in SUITS.values()) {
+                print("${suit.Symbol} ")
+        }
+        println("\n")
+
+        for(suit in SUITS.values())
+                for(rank in RANKS.values()) {
+                        print("${rank.Symbol}${suit.Symbol} ")
+                }
 }
