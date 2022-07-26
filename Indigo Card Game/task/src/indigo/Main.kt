c0a7d94729e99d38de4c24f75fefbdb1a4bd2af2
Treboder <robert.kohn@gmx.net>
2022-07-26 19:14:31 +0200

    Stage 3/5: The game is started (test passed)

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -2,23 +2,66 @@ import kotlin.random.Random
 
 fun main() {
 
-        do {
-                print("Choose an action (reset, shuffle, get, exit): \n> ")
-                var command = readLine()!!
-                when(command) {
-                        "reset" -> Deck.reset()
-                        "shuffle" -> Deck.shuffle()
-                        "get" -> Deck.get()
-                        "print" -> Deck.printCardList(Deck.cards)
-                        "exit" -> break
-                        else -> println("Wrong action.")
+        println("Indigo Card Game")
+        var humanPlaysFirst:Boolean? = null
+        while(humanPlaysFirst == null) {
+                print("Play first?\n> ")
+                var answer = readLine()!!
+                if( answer == "yes")
+                        humanPlaysFirst = true
+                else if (answer == "no")
+                        humanPlaysFirst = false
+        }
+
+        Deck.shuffle()
+        var cardsOnTheTable = Deck.get(4)
+        print("Initial cards on the table: ")
+        Deck.printCardList(cardsOnTheTable)
+
+        var human = Player()
+        human.cards = Deck.get(6)
+
+        var computer = Player()
+        computer.cards = Deck.get(6)
+
+        fun Int.isEven(): Boolean {
+                if(this % 2 == 0)
+                        return true
+                else
+                        return false
+        }
+        var round = if(humanPlaysFirst) 0 else 1 // even for humans turn and odd for computers turn
+        while(cardsOnTheTable.size < 52)
+        {
+                println()
+                println("${cardsOnTheTable.size} cards on the table, and the top card is ${cardsOnTheTable.last()}")
+                if(round.isEven()) {
+                        print("Cards in hand: ")
+                        human.printCardList()
+
+                        var validCardIndex = 0
+                        while(validCardIndex == 0 || !(validCardIndex in 1..human.cards.size)) {
+                                print("Choose a card to play (1-${human.cards.size}):\n> ")
+                                var humanCardInput:String = readLine()!!
+
+                                if(humanCardInput == "exit") {println("Game Over"); return }
+
+                                try{ validCardIndex = humanCardInput.toInt()} catch (e:Exception) { validCardIndex = 0}
+                        }
+                        cardsOnTheTable.add(human.throwCardFromHand(validCardIndex))
+                        human.getSixNewCardsFromDeckIfHandIsEmpty()
                 }
-        } while(command != "exit")
-        println("Bye")
-
+                else {
+                        cardsOnTheTable.add(computer.throwCardFromHandDefaultFirst())
+                        println("Computer plays ${cardsOnTheTable.last()}")
+                        computer.getSixNewCardsFromDeckIfHandIsEmpty()
+                }
+                round++
+        }
+        println()
+        println("52 cards on the table, and the top card is ${cardsOnTheTable.last()}")
+        println("Game Over")
 }
-
-
 
 object Deck {
 
@@ -49,22 +92,12 @@ object Deck {
                         cards[cardIndex1] =  cards[cardIndex2]
                         cards[cardIndex2] =  swap
                 }
-                println("Card deck is shuffled.")
         }
 
-        fun get() {
+        fun get(numberOfCardsToGet:Int): MutableList<Card> {
                 var rnd = Random
-                print("Number of cards: \n> ")
-                var numberOfCardsToGet = 0
-                try {
-                        numberOfCardsToGet = readLine()!!.toInt()
-                } catch (e:Exception) {
-                        println("Invalid number of cards.")
-                        return
-                }
-
                 var selectedCards = mutableListOf<Card>()
-                if(numberOfCardsToGet in 1..52) {
+                if(numberOfCardsToGet!! in 1..52) {
                         if(numberOfCardsToGet <= cards.count()) {
                                 while(selectedCards.count() < numberOfCardsToGet) {
                                         var index = rnd.nextInt(0,cards.count())
@@ -78,7 +111,7 @@ object Deck {
                 else
                         println("Invalid number of cards.")
 
-                printCardList(selectedCards)
+                return selectedCards
         }
 
         fun printCardList(cardListToPrint:MutableList<Card>) {
@@ -92,6 +125,55 @@ class Card(var suit:SUITS, var rank:RANKS) {
 
         fun print() {
                 print("${rank.Symbol}${suit.Symbol} ")
+        }
+
+        override fun toString():String {
+                return "${rank.Symbol}${suit.Symbol}"
+        }
+}
+
+class Player() {
+        var cards = mutableListOf<Card>()
+
+        fun throwCardFromHand(cardToThrowIndex:Int): Card {
+                var cardToThrowObject = cards[cardToThrowIndex-1]
+                cards.removeAt(cardToThrowIndex-1)
+                return cardToThrowObject
+        }
+
+        fun throwCardFromHandDefaultFirst(): Card {
+                if(cards.size == 1) {
+                        var lastCard = cards[0]
+                        cards.clear()
+                        return lastCard
+                }
+                var cardToThrowObject = cards.last()
+                cards.removeLast()
+                return cardToThrowObject
+        }
+
+        fun throwCardFromHandRandomly(): Card {
+                if(cards.size == 1) {
+                        var lastCard = cards[0]
+                        cards.clear()
+                        return lastCard
+                }
+                var rnd = Random
+                var cardToThrowIndex = rnd.nextInt(0,cards.size)
+                var cardToThrowObject = cards[cardToThrowIndex]
+                cards.removeAt(cardToThrowIndex)
+                return cardToThrowObject
+        }
+
+        fun getSixNewCardsFromDeckIfHandIsEmpty() {
+                if(cards.size == 0 && Deck.cards.size >=6)
+                        cards.addAll(Deck.get(6))
+        }
+
+        fun printCardList() {
+                for(i in 0..cards.size-1)
+                        print("${i+1})${cards[i]} ")
+                println()
         }
 }
 
@@ -116,23 +198,4 @@ enum class SUITS(val CodePoint: Char, val Symbol:String, Rank: Int) {
         SPADE('\u2660', Character.toString('\u2660'), 1),
         HEART('\u2665', Character.toString('\u2665'), 2),
         DIAMOND('\u2666', Character.toString('\u2666'), 3)
-}
-
-fun stage1() {
-        // https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
-
-        for(rank in RANKS.values()) {
-                print("${rank.Symbol} ")
-        }
-        println("\n")
-
-        for(suit in SUITS.values()) {
-                print("${suit.Symbol} ")
-        }
-        println("\n")
-
-        for(suit in SUITS.values())
-                for(rank in RANKS.values()) {
-                        print("${rank.Symbol}${suit.Symbol} ")
-                }
 }
